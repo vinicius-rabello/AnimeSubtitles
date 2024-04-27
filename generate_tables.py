5be92b1b56e4c247768f06635126f6f8a7491c5b
Felipe Patitucci <felipepatitucci@gmail.com>
2024-04-26 22:25:08 -0300

    fixed formatting

--- generate_tables.py
+++ generate_tables.py
@@ -2,31 +2,34 @@ import os
 import pandas as pd
 import ass
 
-animes=os.listdir('data') # get every anime folder
-for anime in animes: # iterate over every anime in data folder
-    folder_path='data/'+anime+'/processed' 
-    episodes=os.listdir(folder_path) # list of every .ass file in anime folder
-    
+animes = os.listdir('data')  # get every anime folder
+for anime in animes:  # iterate over every anime in data folder
+    folder_path = 'data/' + anime + '/processed'
+    # list of every .ass file in anime folder
+    episodes = os.listdir(folder_path)
+
     if f'{anime}.xlsx' in os.listdir(f'data/{anime}'):
-        print(f'{anime}.xlsx already exists!' )
+        print(f'{anime}.xlsx already exists!')
         continue
 
-    table=[]
-    for episode in episodes: # iterate over every episode
-        path=folder_path+'/'+episode
-        episode_number=episode.split('.')[0].split('_')[-1] # get episode number (episode title is always anime_name_{episode_num}.ass)
+    table = []
+    for episode in episodes:  # iterate over every episode
+        path = folder_path + '/' + episode
+        # get episode number (episode title is always anime_name_{episode_num}.ass)
+        episode_number = episode.split('.')[0].split('_')[-1]
 
         try:
             with open(path, encoding='utf_8_sig') as f:
-                doc = ass.parse(f) # read .ass file
-                events=doc.events # get every dialogue line
+                doc = ass.parse(f)  # read .ass file
+                events = doc.events  # get every dialogue line
                 print(f'reading {path}')
                 for event in events:
-                    table.append([episode_number,event.name,event.text]) # save every line with whoever said the line 
-                                                                        # and the episode number 
-        except:
+                    # save every line with whoever said the line
+                    table.append([episode_number, event.name, event.text])
+                    # and the episode number
+        except Exception:
             print(f'error reading {path}')
-                                                            
-    df=pd.DataFrame(table,columns=['episode_number','name','text'])
-    df.to_excel(f'data/{anime}/{anime}.xlsx',index=False)
+
+    df = pd.DataFrame(table, columns=['episode_number', 'name', 'text'])
+    df.to_excel(f'data/{anime}/{anime}.xlsx', index=False)
     print(f'{anime}.xlsx created successfuly!')
--- read_subtitles.py
+++ read_subtitles.py
@@ -1,24 +1,25 @@
 import lzma
 import os
 
-animes=os.listdir('data') # get every anime folder
+animes = os.listdir('data')  # get every anime folder
 for anime in animes:
 
     print(f'\nExcracting subtitles from {anime}\n')
 
-    folder_path='data/'+anime+'/raw'
-    episodes=os.listdir(folder_path)
+    folder_path = 'data/'+anime+'/raw'
+    episodes = os.listdir(folder_path)
 
-    for episode in episodes: # iterate over every file in anime folder
-        
-        path=folder_path+'/'+episode 
+    for episode in episodes:  # iterate over every file in anime folder
 
-        with lzma.open(path, mode='rb') as file: # read .xz file
-            content=file.read()
+        path = folder_path + '/' + episode
 
-        path=path[:-3]
-        path=path.replace('raw','processed') # we want to save .txt files into processed folder, not raw
+        with lzma.open(path, mode='rb') as file:  # read .xz file
+            content = file.read()
 
-        with open(path+'.ass','wb') as file: # write content into .txt file
+        path = path[:-3]
+        # we want to save .txt files into processed folder, not raw
+        path = path.replace('raw', 'processed')
+
+        with open(path+'.ass', 'wb') as file:  # write content into .txt file
             file.write(content)
             print(f'{path}.ass successfuly created!')
